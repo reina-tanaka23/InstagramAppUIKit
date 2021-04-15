19969991a5dc66f76b05d657acffabc58575b4d0
reina.tanaka <reina.tanaka0223@gmail.com>
2021-04-15 15:33:33 +0900

    completed

---
+++ InstagramAppUIKit/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  InstagramAppUIKit
+//
+//  Created by reina.tanaka on 2021/04/13.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
